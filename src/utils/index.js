9f23f04b87c01eb4a6633852d00bc682a27d6b28
Autumn <439661734@qq.com>
2022-03-27 09:26:28 +0800

    feat: a method for ajax request

---
+++ src/utils/index.js
@@ -0,0 +1,22 @@
+export function request({
+  url,
+  method = 'get',
+  data,
+  headers = {}
+}) {
+  return new Promise((resolve) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open(method, url)
+
+    Object.keys(headers).forEach((key) => {
+      xhr.setRequestHeader(key, headers[key])
+    })
+
+    xhr.send(data)
+    xhr.onload = (e) => {
+      resolve({
+        data: e.target.response
+      })
+    }
+  })
+}
